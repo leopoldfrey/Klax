ccd733b23ccdb32b742d2886bca03e5dd1353664
Léopold Frey <leopold.frey@lilo.org>
2025-07-03 16:28:23 +0200

    corrections

--- klax_looper/start_klax.sh
+++ klax_looper/start_klax.sh
@@ -2,6 +2,8 @@
 
 export DISPLAY=:0
 
+sleep 5
+
 # Aller dans le dossier de l'app
 cd /home/klax/Documents/GitHub/Klax/klax_looper
 
@@ -10,7 +12,7 @@ source venv/bin/activate
 
 # Démarrer le patch Pure Data en mode nogui
 #pd -nogui looper.pd &
-pd looper.pd &
+pd -nogui looper.pd &
 
 # Démarrer le script Python
 python klax_midi.py
